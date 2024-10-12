7400c7079feb1d0f1837535b47623bba31336a11
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-12 17:19:06 +0200

    feat: add new generic for the Model ID

--- src/main/java/sap/ass01/layered/persistence/Repository.java
+++ src/main/java/sap/ass01/layered/persistence/Repository.java
@@ -2,7 +2,9 @@ package sap.ass01.layered.persistence;
 
 import java.io.Serializable;
 
-public interface Repository<T extends SerializableEntity<?>> {
+public interface Repository<T extends SerializableEntity<?>, I> {
     // TODO: CRUD operations
     void saveAll(Iterable<T> objects);
+    void save(T object);
+    void getById(I id);
 }
---
+++ src/main/java/sap/ass01/layered/persistence/UserRepository.java
@@ -0,0 +1,4 @@
+package sap.ass01.layered.persistence;
+
+public class UserRepository implements Repository<Void, String>{
+}
