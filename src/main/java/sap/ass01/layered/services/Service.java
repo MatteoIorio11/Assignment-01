518e606b94712ba70521722b777d458ca509227c
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-12 17:19:35 +0200

    feat: add Repository inside UserService

--- src/main/java/sap/ass01/layered/services/Service.java
+++ src/main/java/sap/ass01/layered/services/Service.java
@@ -1,5 +1,7 @@
 package sap.ass01.layered.services;
 
 public interface Service<X> {
+    //TODO: A specific service interact with a specific GUI
+    // TODO: at each operation done, a possible operation from a repository can be done
     void add(X newValue);
 }
--- src/main/java/sap/ass01/layered/services/UserService.java
+++ src/main/java/sap/ass01/layered/services/UserService.java
@@ -2,12 +2,15 @@ package sap.ass01.layered.services;
 
 import sap.ass01.layered.business.User;
 import sap.ass01.layered.persistence.Repository;
+import sap.ass01.layered.persistence.UserRepository;
+
 import java.util.List;
 
 public class UserService implements Service<User>{
     List<User> userList;
+    final Repository<User> userRepository = new UserRepository();
     @Override
     public void add(User newValue) {
-
+        userRepository.saveAll(userList);
     }
 }
