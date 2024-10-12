a02ba483a52de251e20f1fdf6d7180c779dda467
S-furi <stefano.furi@studio.unibo.it>
2024-10-12 16:34:09 +0200

    chore: make `SerializableEntity` generic

--- src/main/java/sap/ass01/layered/persistence/SerializableEntity.java
+++ src/main/java/sap/ass01/layered/persistence/SerializableEntity.java
@@ -2,6 +2,10 @@ package sap.ass01.layered.persistence;
 
 import java.io.Serializable;
 
-public interface SerializableEntity extends Serializable {
-    String getSerializedObject();
+/**
+ * A serializable entity in the business domain.
+ * @param <O> the serialized value type of this object.
+ */
+public interface SerializableEntity<O> extends Serializable {
+    O getSerializedObject();
 }
