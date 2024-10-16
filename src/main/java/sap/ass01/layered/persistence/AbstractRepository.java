9b21446983d494f136baae68101ec3ecbc8ff7f3
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-16 10:24:31 +0200

    feat: implement the abstract serializer with the injected Serializer

--- src/main/java/sap/ass01/layered/persistence/AbstractRepository.java
+++ src/main/java/sap/ass01/layered/persistence/AbstractRepository.java
@@ -1,2 +1,29 @@
-package sap.ass01.layered.persistence;public class AbstractRepository {
+package sap.ass01.layered.persistence;
+
+import java.util.Optional;
+
+public class AbstractRepository<T extends SerializableEntity<?>, K> implements Repository<T, K> {
+    private final Serializer<T, K> serializer;
+    public AbstractRepository(final Serializer<T, K> serializer){
+        this.serializer = serializer;
+    }
+    @Override
+    public void saveAll(Iterable<T> objects) {
+        this.serializer.serializeAll(objects);
+    }
+
+    @Override
+    public void save(T object) {
+        this.serializer.serialize(object);
+    }
+
+    @Override
+    public Iterable<T> getAll() {
+        return this.serializer.readAll();
+    }
+
+    @Override
+    public Optional<T> getObject(K objectKey) {
+        return this.serializer.readByID(objectKey);
+    }
 }
