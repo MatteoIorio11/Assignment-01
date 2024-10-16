4389e492ccac2badfdcb172353436e1534bf75ba
S-furi <stefano.furi@studio.unibo.it>
2024-10-16 10:49:42 +0200

    chore(presentation): make object specific observers as interfaces

--- src/main/java/sap/ass01/layered/presentation/observers/EBikeObserver.java
+++ src/main/java/sap/ass01/layered/presentation/observers/EBikeObserver.java
@@ -2,10 +2,7 @@ package sap.ass01.layered.presentation.observers;
 
 import sap.ass01.layered.business.EBike;
 
-public class EBikeObserver implements InputObserver<EBike> {
-
+public interface EBikeObserver extends InputObserver<EBike> {
     @Override
-    public void notifyUpdateRequested(final EBike newValue) {
-
-    }
+    void notifyUpdateRequested(final EBike newValue);
 }
