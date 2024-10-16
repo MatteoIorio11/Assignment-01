54c7c45cee9a43c7cbdcce06eba8711f449dfca7
S-furi <stefano.furi@studio.unibo.it>
2024-10-16 10:32:22 +0200

    feat(presentation): create observer

---
+++ src/main/java/sap/ass01/layered/presentation/observers/UserObserver.java
@@ -0,0 +1,11 @@
+package sap.ass01.layered.presentation.observers;
+
+import sap.ass01.layered.business.User;
+
+public class UserObserver implements InputObserver<User> {
+
+    @Override
+    public void notifyUpdateRequested(User newValue) {
+
+    }
+}
