5e44dbd1d8c3bc2e769159a98c3ee70282c5db15
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-17 15:02:46 +0200

    feat: add all the implementations of all the Serializer interface

--- src/main/java/sap/ass01/layered/persistence/json/JacksonSerializer.java
+++ src/main/java/sap/ass01/layered/persistence/json/JacksonSerializer.java
@@ -1,82 +1,89 @@
 package sap.ass01.layered.persistence.json;
 
+import com.fasterxml.jackson.annotation.JsonTypeId;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import sap.ass01.layered.business.User;
+import sap.ass01.layered.business.UserImpl;
 import sap.ass01.layered.persistence.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Predicate;
 
-public class JacksonSerializer<T extends SerializableEntity<?>, K> implements Serializer<T, K> {
-    private final Map<Class<?>, File> files:
-    private final String destinationPath = "src/main/resources/";
+public class JacksonSerializer<T, K> implements Serializer<T, K>{
+    private final File file;
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final Class<T> typeClass;
 
-    public JacksonSerializer() {
-        this.files = new HashMap<>();
-
+    public JacksonSerializer(final Class<T> clazz) {
+        this.file = new File(Serializer.path() + clazz.getSimpleName().toLowerCase() + ".json");
+        this.typeClass = clazz;
+        this.checkFile();
     }
+
     @Override
     public void serialize(T object) {
+        final List<T> myObjects = Serializer.iterableToList(this.readAll());
+        System.out.println(myObjects);
+        myObjects.add(object);
+        System.out.println(myObjects);
+        this.serializeAll(myObjects);
+    }
+
+    @Override
+    public void serializeAll(Iterable<T> objects) {
         try {
-            this.checkFile(object);
-            final List<T> myObjects = objectMapper.readValue(this.files.get(object.getClass()), new TypeReference<List<T>>(){});
-            myObjects.add(object);
-            this.serializeAll(myObjects);
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(this.file, objects);
         }catch (IOException exception) {
             System.err.println(exception.getMessage());
         }
     }
 
     @Override
-    public void serializeAll(Iterable<T> objects) {
-        final Optional<T> object = this.getClassFromIterable(objects);
-        if (object.isPresent()) {
-            try {
-                this.checkFile(object.get());
-                objectMapper.writerWithDefaultPrettyPrinter().writeValue(this.files.get(object.get().getClass()), objects);
-            }catch (IOException exception) {
-                System.err.println(exception.getMessage());
-            }
-        }
-    }
-
-    @Override
     public Iterable<T> readAll() {
-        return null;
+        try {
+            return objectMapper.readValue(this.file,
+                    objectMapper.getTypeFactory().constructCollectionType(List.class, this.typeClass));
+        }catch (IOException exception) {
+            System.err.println(exception.getMessage());
+        }
+        return new LinkedList<T>();
     }
 
     @Override
-    public Optional<T> readByID(final K id) {
+    public Optional<T> readByID(K key) {
+        final Iterable<T> output = this.readAll();
+        for (final T obj : output) {
+            try {
+                System.out.println(obj.getClass());
+                final Method[] methods = obj.getClass().getDeclaredMethods();
+                for (final Method method : methods) {
+                    if (method.isAnnotationPresent(Key.class)) {
+                        final Object objectKey = method.invoke(obj);
+                        if (Objects.nonNull(objectKey) && objectKey.equals(key)) {
+                            return Optional.of(obj);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         return Optional.empty();
     }
 
-    private void checkFile(final T object) {
-        final String className = object.getClass().getSimpleName();
-        final File file = new File(this.destinationPath + className + ".json");
-        if (!Files.exists(Paths.get(file.getPath()))) {
+    private void checkFile() {
+        if (!Files.exists(Paths.get(this.file.getPath()))) {
             try {
-                Files.createDirectories(Paths.get(file.getPath()));
-                this.files.put(object.getClass(), file);
+                Files.createFile(Paths.get(this.file.getPath()));
             }catch (IOException exception) {
                 System.err.println(exception.getMessage());
             }
         }
     }
-
-    private Optional<T> getClassFromIterable(final Iterable<T> iterable) {
-        for (final T element : iterable) {
-            if (element != null) {
-                return Optional.of(element);
-            }
-        }
-        return Optional.empty();
-    }
-
 }
