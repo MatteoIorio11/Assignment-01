baeb915aa2ab533ccdcd4edb50ba85957d558c3d
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-17 15:01:45 +0200

    feat: add serializer interface, set all the methods for serialise and read values

--- src/main/java/sap/ass01/layered/persistence/Serializer.java
+++ src/main/java/sap/ass01/layered/persistence/Serializer.java
@@ -1,10 +1,25 @@
 package sap.ass01.layered.persistence;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
-public interface Serializer<T extends SerializableEntity<?>, K> {
+public interface Serializer<T, K> {
     void serialize(T object);
     void serializeAll(Iterable<T> objects);
     Iterable<T> readAll();
-    Optional<T> readByID(K id);
+    Optional<T> readByID(K key);
+
+    static String path() {
+        return "src/main/resources/";
+    }
+
+    static <T> List<T> iterableToList(Iterable<T> iterable) {
+        final List<T> list = new LinkedList<>();
+        for (final T item : iterable) {
+            list.add(item);
+        }
+        return list;
+    }
 }
