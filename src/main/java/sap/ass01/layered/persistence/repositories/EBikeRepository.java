2777ad9bc743a0cd8074e2ffc5fdfd1cc79b55fa
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-17 16:03:49 +0200

    refactor: remove unsuded imports

--- src/main/java/sap/ass01/layered/persistence/repositories/EBikeRepository.java
+++ src/main/java/sap/ass01/layered/persistence/repositories/EBikeRepository.java
@@ -1,13 +1,10 @@
 package sap.ass01.layered.persistence.repositories;
 
 import sap.ass01.layered.business.EBikeImpl;
-import sap.ass01.layered.business.UserImpl;
 import sap.ass01.layered.persistence.AbstractRepository;
 import sap.ass01.layered.persistence.Serializer;
-import sap.ass01.layered.persistence.json.JacksonSerializer;
 
 public class EBikeRepository extends AbstractRepository<EBikeImpl, String> {
-
     public EBikeRepository(Serializer<EBikeImpl, String> serializer) {
         super(serializer);
     }
