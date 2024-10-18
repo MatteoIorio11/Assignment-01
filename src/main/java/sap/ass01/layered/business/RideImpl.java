039dded114011a340f851bceae8ecfd0e6e48cf0
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-18 10:44:55 +0200

    feat: add JSON annotation for serialization

--- src/main/java/sap/ass01/layered/business/RideImpl.java
+++ src/main/java/sap/ass01/layered/business/RideImpl.java
@@ -1,27 +1,54 @@
 package sap.ass01.layered.business;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import sap.ass01.layered.presentation.RideSimulationControlPanel;
 import sap.ass01.layered.presentation.EBikeApp;
 
 import java.util.Date;
 import java.util.Optional;
 
+@JsonIgnoreProperties(ignoreUnknown = true)  // Ignore unknown properties like "available"
 public class RideImpl implements Ride {
-
+	@JsonProperty("startDate")
 	private Date startedDate;
+	@JsonProperty("endDate")
 	private Optional<Date> endDate;
+	@JsonProperty("user")
 	private User user;
+	@JsonProperty("bike")
 	private EBike ebike;
+	@JsonProperty("ongoing")
 	private boolean ongoing;
+	@JsonProperty("id")
 	private String id;
+	@JsonIgnore
 	private RideSimulation rideSimulation;
-	
+
 	public RideImpl(String id, User user, EBike ebike) {
 		this.id = id;
 		this.startedDate = new Date();
 		this.endDate = Optional.empty();
 		this.user = user;
 		this.ebike = ebike;
+	}
+
+	@JsonCreator
+	public RideImpl(
+			@JsonProperty("startDate") Date startedDate,
+			@JsonProperty("endDate") Optional<Date> endDate,
+			@JsonProperty("user") User user,
+			@JsonProperty("bike") EBike ebike,
+			@JsonProperty("ongoing") boolean ongoing,
+			@JsonProperty("id") String id) {
+		this.startedDate = startedDate;
+		this.endDate = endDate;
+		this.user = user;
+		this.ebike = ebike;
+		this.ongoing = ongoing;
+		this.id = id;
 	}
 	
 
