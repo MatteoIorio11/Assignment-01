6974b5e219f1fddb460763389fbd7c6546f866d7
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-18 10:58:32 +0200

    feat: create new generic interface for data communication

--- src/main/java/sap/ass01/layered/persistence/EntityStore.java
+++ src/main/java/sap/ass01/layered/persistence/EntityStore.java
@@ -1,2 +1,11 @@
-package sap.ass01.layered.persistence;public interface EntityStore {
+package sap.ass01.layered.persistence;
+
+import java.util.Optional;
+
+public interface EntityStore<T, K> {
+    void serialize(T object);
+    void serializeAll(Iterable<T> objects);
+    Iterable<T> readAll();
+    Optional<T> readByID(K key);
+
 }
