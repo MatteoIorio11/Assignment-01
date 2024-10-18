3d919f3495706400357cfa88fc0a4a124d53d859
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-18 10:51:21 +0200

    feat: add new RideRepository

--- src/main/java/sap/ass01/layered/persistence/repositories/RideRepository.java
+++ src/main/java/sap/ass01/layered/persistence/repositories/RideRepository.java
@@ -1,2 +1,11 @@
-package sap.ass01.layered.persistence.repositories;public class RideRepository {
+package sap.ass01.layered.persistence.repositories;
+
+import sap.ass01.layered.business.Ride;
+import sap.ass01.layered.persistence.AbstractRepository;
+import sap.ass01.layered.persistence.Serializer;
+
+public class RideRepository extends AbstractRepository<Ride, String> {
+    public RideRepository(Serializer<Ride, String> serializer) {
+        super(serializer);
+    }
 }
