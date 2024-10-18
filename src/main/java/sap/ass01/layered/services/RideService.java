45bd822e4ff19222170ae2a13237ee8cd746ef63
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-18 18:26:25 +0200

    chore: add new implementation for ride creation, set types for repo objects

--- src/main/java/sap/ass01/layered/services/RideService.java
+++ src/main/java/sap/ass01/layered/services/RideService.java
@@ -1,16 +1,23 @@
 package sap.ass01.layered.services;
 
+import sap.ass01.layered.business.EBike;
 import sap.ass01.layered.business.Ride;
+import sap.ass01.layered.business.RideImpl;
+import sap.ass01.layered.business.User;
 import sap.ass01.layered.persistence.Repository;
 import sap.ass01.layered.presentation.observers.InputObserver;
 import sap.ass01.layered.services.dto.RideDTO;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class RideService implements Service<Ride>, InputObserver<RideDTO> {
 
-    private final List<Repository<Ride, ?>> repositories;
+    private final List<Repository<Ride, String>> repositories;
+    private Repository<User, String> userRepository;
+    private Repository<EBike, String> eBikeRepository;
+
 
     public RideService() {
         this.repositories = new ArrayList<>();
@@ -18,7 +25,16 @@ public class RideService implements Service<Ride>, InputObserver<RideDTO> {
 
     @Override
     public void notifyUpdateRequested(final RideDTO newValue) {
-        // Logic for create a new ride given the DTO
+        final Optional<User> user = this.userRepository.getObjectByID(newValue.userId());
+        final Optional<EBike> eBike = this.eBikeRepository.getObjectByID(newValue.bikeId());
+        if (user.isPresent() && eBike.isPresent()) {
+            this.repositories.forEach(repo -> {
+                final String id = repo.generateNewId();
+                final Ride newRide = new RideImpl(id, user.get(), eBike.get());
+                repo.save(newRide);
+            });
+        }
+
     }
 
     @Override
