6e366eb978fcb606c916baafa40d84203fb920c5
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-18 10:45:21 +0200

    refactor: use new functions, set correct generic type for serializers

--- src/test/java/it/unibo/sap/persistence/jackson/UserSerializeTest.java
+++ src/test/java/it/unibo/sap/persistence/jackson/UserSerializeTest.java
@@ -20,8 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserSerializeTest {
-    private final Serializer<UserImpl, String> serializer = new JacksonSerializer<>(UserImpl.class);
-    private final UserRepository userRepository = new UserRepository(serializer);
+    private final UserRepository userRepository = new UserRepository(Serializer.userJSONSerializer());
 
     @BeforeEach
     public void deleteFiles() {
@@ -29,26 +28,26 @@ public class UserSerializeTest {
     }
     @Test
     public void testSerialiseUser() {
-        final UserImpl user = new UserImpl("miao");
+        final User user = new UserImpl("miao");
         this.userRepository.save(user);
-        final Optional<UserImpl> fileUser = this.userRepository.getObjectByID("miao");
+        final Optional<User> fileUser = this.userRepository.getObjectByID("miao");
         assertTrue(fileUser.isPresent());
         assertEquals(fileUser.get(), user);
     }
 
     @Test
     public void testSerialiseUsers() {
-        final UserImpl user1 = new UserImpl("miao1");
-        final UserImpl user2 = new UserImpl("miao2");
-        final UserImpl user3 = new UserImpl("miao3");
+        final User user1 = new UserImpl("miao1");
+        final User user2 = new UserImpl("miao2");
+        final User user3 = new UserImpl("miao3");
 
-        final List<UserImpl> users = List.of(
+        final List<User> users = List.of(
                 user1,
                 user2,
                 user3
         );
         this.userRepository.saveAll(users);
-        final Optional<UserImpl> fileUser = this.userRepository.getObjectByID("miao2");
+        final Optional<User> fileUser = this.userRepository.getObjectByID("miao2");
         assertTrue(fileUser.isPresent());
         assertEquals(fileUser.get(), user2);
     }
