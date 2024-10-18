679ce58e1ce18c8ef67cfc8af9ede4bd5788e36c
S-furi <stefano.furi@studio.unibo.it>
2024-10-18 17:37:30 +0200

    feat(service): make service use corresponding repositories

--- src/main/java/sap/ass01/layered/services/Service.java
+++ src/main/java/sap/ass01/layered/services/Service.java
@@ -1,10 +1,10 @@
 package sap.ass01.layered.services;
 
-import sap.ass01.layered.presentation.observers.InputObserver;
-import sap.ass01.layered.services.dto.DTO;
+import sap.ass01.layered.persistence.Repository;
 
 public interface Service<T> {
     // TODO: A specific service interact with a specific GUI
     // TODO: at each operation done, a possible operation from a repository can be done
     void add(T newValue);
+    <R extends Repository<T, ?>> void addRepository(R repository);
 }
--- src/main/java/sap/ass01/layered/services/UserService.java
+++ src/main/java/sap/ass01/layered/services/UserService.java
@@ -1,19 +1,33 @@
 package sap.ass01.layered.services;
 
 import sap.ass01.layered.business.User;
+import sap.ass01.layered.persistence.Repository;
 import sap.ass01.layered.presentation.observers.InputObserver;
 import sap.ass01.layered.services.dto.UserDTO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService implements Service<User>, InputObserver<UserDTO> {
 
-    public UserService() { }
+    private final List<Repository<User, ?>> repositories;
+
+    public UserService() {
+        this.repositories = new ArrayList<>();
+    }
 
     @Override
     public void notifyUpdateRequested(final UserDTO newValue) {
+        // Some logic for retrieving user given the dto
     }
 
     @Override
     public void add(final User newValue) {
+        this.repositories.forEach(r -> r.save(newValue));
+    }
 
+    @Override
+    public <R extends Repository<User, ?>> void addRepository(final R repository) {
+        this.repositories.add(repository);
     }
 }
