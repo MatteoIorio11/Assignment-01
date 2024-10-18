31611dc1c911a6947e8542b47540f64a55964f43
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-18 10:41:09 +0200

    refacctor: user Interface instead of Implementation

--- src/main/java/sap/ass01/layered/persistence/repositories/EBikeRepository.java
+++ src/main/java/sap/ass01/layered/persistence/repositories/EBikeRepository.java
@@ -1,11 +1,18 @@
 package sap.ass01.layered.persistence.repositories;
 
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.module.SimpleAbstractTypeResolver;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import sap.ass01.layered.business.EBike;
 import sap.ass01.layered.business.EBikeImpl;
+import sap.ass01.layered.business.User;
+import sap.ass01.layered.business.UserImpl;
 import sap.ass01.layered.persistence.AbstractRepository;
 import sap.ass01.layered.persistence.Serializer;
 
 public class EBikeRepository extends AbstractRepository<EBikeImpl, String> {
-    public EBikeRepository(Serializer<EBikeImpl, String> serializer) {
+    public EBikeRepository(final Serializer<EBikeImpl, String> serializer) {
         super(serializer);
     }
+
 }
--- src/main/java/sap/ass01/layered/persistence/repositories/UserRepository.java
+++ src/main/java/sap/ass01/layered/persistence/repositories/UserRepository.java
@@ -1,14 +1,15 @@
 package sap.ass01.layered.persistence.repositories;
 
 import sap.ass01.layered.business.EBikeImpl;
+import sap.ass01.layered.business.User;
 import sap.ass01.layered.business.UserImpl;
 import sap.ass01.layered.persistence.AbstractRepository;
 import sap.ass01.layered.persistence.Serializer;
 import sap.ass01.layered.persistence.json.JacksonSerializer;
 
 
-public class UserRepository extends AbstractRepository<UserImpl, String> {
-    public UserRepository(Serializer<UserImpl, String> serializer) {
+public class UserRepository extends AbstractRepository<User, String> {
+    public UserRepository(Serializer<User, String> serializer) {
         super(serializer);
     }
 }
