50643857d9dc75fa3e3325c75227b55b3bd349f0
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-19 11:31:48 +0200

    feat: add new generic type inside the interface, this type it is used for the key class type

--- src/main/java/sap/ass01/layered/services/Service.java
+++ src/main/java/sap/ass01/layered/services/Service.java
@@ -2,9 +2,9 @@ package sap.ass01.layered.services;
 
 import sap.ass01.layered.persistence.Repository;
 
-public interface Service<T> {
+public interface Service<T, K> {
     // TODO: A specific service interact with a specific GUI
     // TODO: at each operation done, a possible operation from a repository can be done
     void add(T newValue);
-    <R extends Repository<T, ?>> void addRepository(R repository);
+    <R extends Repository<T, K>> void addRepository(R repository);
 }
