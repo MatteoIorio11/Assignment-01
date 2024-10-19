8e0330f3bcbb44a3a90e6957a57650bdc8d9fbf9
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-19 19:47:24 +0200

    feat: add new method update

--- src/main/java/sap/ass01/layered/persistence/EntityStore.java
+++ src/main/java/sap/ass01/layered/persistence/EntityStore.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 public interface EntityStore<T, K> {
     void serialize(T object);
     void serializeAll(Iterable<T> objects);
+    void update(T object);
     Iterable<T> readAll();
     Optional<T> readByID(K key);
 
