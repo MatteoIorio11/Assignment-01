fe9a38508daf8e8e274a57bab7448952a0ca4b87
S-furi <stefano.furi@studio.unibo.it>
2024-10-19 19:29:22 +0200

    chore: remove unused method

--- src/main/java/sap/ass01/layered/services/dto/DTO.java
+++ src/main/java/sap/ass01/layered/services/dto/DTO.java
@@ -1,14 +1,5 @@
 package sap.ass01.layered.services.dto;
 
-import java.util.Collection;
-
 public interface DTO {
     Object getId();
-
-    static <D extends DTO> Iterable<D> filterIsInstance(final Collection<DTO> c, Class<D> clazz) {
-        return c.stream()
-                .filter(clazz::isInstance)
-                .map(clazz::cast)
-                .toList();
-    }
 }
