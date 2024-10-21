28d4282e58f7e99ecb43e210fc9e25fe01b7f1b4
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-21 18:25:59 +0200

    feat: use the model for saving/updating operations

--- src/main/java/sap/ass01/layered/services/AbstractObserverService.java
+++ src/main/java/sap/ass01/layered/services/AbstractObserverService.java
@@ -1,24 +1,23 @@
 package sap.ass01.layered.services;
 
+import sap.ass01.layered.business.Business;
 import sap.ass01.layered.business.observers.ModelObserver;
 import sap.ass01.layered.business.observers.ModelObserverSource;
 import sap.ass01.layered.persistence.Repository;
 import sap.ass01.layered.services.dto.DTO;
 import sap.ass01.layered.services.observers.InputObserver;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-public abstract class AbstractObserverService<D extends DTO, T> implements Service<T, String>, InputObserver<D>, ModelObserverSource {
-
+public abstract class AbstractObserverService<D extends DTO, T extends Business<T, String>> implements Service<T, String>, InputObserver<D>, ModelObserverSource {
     private final List<ModelObserver> observers;
+    private final List<T> objects;
     protected final List<Repository<T, String>> repositories;
 
     public AbstractObserverService() {
         this.observers = new ArrayList<>();
         this.repositories = new ArrayList<>();
+        this.objects = new LinkedList<>();
     }
 
     @Override
@@ -33,24 +32,34 @@ public abstract class AbstractObserverService<D extends DTO, T> implements Servi
 
     @Override
     public void add(T newValue) {
-        this.repositories.forEach(r -> r.save(newValue));
+        newValue.save();
+        this.objects.add(newValue);
         this.notifyStepDone();
     }
 
     @Override
     public void update(T updatedValue) {
-        this.repositories.forEach(r -> r.update(updatedValue));
+        updatedValue.update();
+        this.objects.removeIf(obj -> obj.equals(updatedValue));
+        this.objects.add(updatedValue);
         this.notifyStepDone();
     }
 
     @Override
     public Iterable<T> getAll() {
-        return this.repositories.stream().findFirst().map(Repository::getAll).orElse(Collections.emptyList());
+        this.objects.clear();
+        this.repositories.stream().findFirst().map(Repository::getAll).orElse(Collections.emptyList()).forEach(this.objects::add);
+        return this.objects;
     }
 
     @Override
-    public Optional<T> getById(String objectId) {
-        return this.repositories.stream().findFirst().flatMap(r -> r.getObjectByID(objectId));
+    public Optional<T> getById(String object) {
+        return this.objects.stream().filter(obj -> obj.getId().equals(object)).findFirst();
+    }
+
+    @Override
+    public List<Repository<T, String>> getRepositories() {
+        return this.repositories;
     }
 
     @Override
